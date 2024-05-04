b66137eae1cc020c2583ad5f665f4e1c36f4f71f
muhsyahrir023 <muh.syahrir@mastersystem.co.id>
2024-05-04 13:55:23 +0700

    Update Login.js

--- src/layouts/auth/Login.js
+++ src/layouts/auth/Login.js
@@ -82,19 +82,20 @@ const Login = () => {
       <Form
         className="shadow p-4 bg-white rounded"
         onSubmit={handleSubmit}
-        style={{ maxWidth: "400px" }}
+        style={{ maxWidth: "90vw", margin: "0 auto" }}
       >
         <div className="text-center">
           <img
             className="img-thumbnail mx-auto mb-2 me-2"
             src={Logo}
             alt="logo"
+            style={{ maxWidth: "40vw" }}
           />
           <img
             className="img-thumbnail mx-auto mb-2"
             src={Logo1}
             alt="logo1"
-            style={{ width: "150px", marginLeft: "10px" }}
+            style={{ maxWidth: "40vw", marginLeft: "10px" }}
           />
         </div>
         <div className="h4 mb-2 text-center mt-2">Corrective Maintenance</div>
