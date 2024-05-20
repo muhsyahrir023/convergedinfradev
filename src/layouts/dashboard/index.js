80b2fb1f971d13634e7bc6bf78539bb92daec6bd
muhsyahrir023 <muh.syahrir@mastersystem.co.id>
2024-05-20 11:21:48 +0700

    Update index.js

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -116,16 +116,37 @@ function Dashboard() {
         <SoftBox mb={3}>
           <Grid container spacing={3}>
             <Grid item xs={12}>
-              <Line data={{
-                labels: Object.keys(monthlyData),
-                datasets: [{
-                  label: 'Monthly Problems',
-                  data: Object.values(monthlyData),
-                  fill: false,
-                  borderColor: 'rgb(75, 192, 192)',
-                  tension: 0.1
-                }]
-              }} />
+              <Line
+  data={{
+    labels: Object.keys(monthlyData),
+    datasets: [{
+      label: 'Monthly Problems',
+      data: Object.values(monthlyData),
+      fill: false,
+      borderColor: 'rgb(75, 192, 192)',
+      tension: 0.1
+    }]
+  }}
+  options={{
+    plugins: {
+      title: {
+        display: true,
+        text: 'Grafik Data Perbulan', // Judul grafik
+        font: {
+          size: 16
+        }
+      },
+      legend: {
+        display: false // Sembunyikan legenda
+      }
+    },
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }}
+/>
             </Grid>
           </Grid>
         </SoftBox>
